6f245738a78a2db2de313d5922ea4a1620f20720
joanna-janos <joanna.janos@wp.pl>
2018-01-21 17:35:21 +0100

    'zaloguj' changes
    Changed logic for 'zaloguj'

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,40 @@ connection.connect();
 connection.query('USE chonotu');
 
 
-
 /* GET home page. */
 
 //http://localhost:3000/?login=Admin
 router.get('/', function(req, res, next) {
-  var post  = {login: req.query.login};
-  var query = connection.query('select * from user where ?', post, function (error, results, fields) {
-    if (error) throw error;
-    res.render('index', { title: results[0].email });   
-  });
-  console.log(query.sql);
+  // var post  = {login: req.query.login};
+  // var query = connection.query('select * from user where ?', post, function (error, results, fields) {
+  //   if (error) throw error;
+  //   res.render('index', { title: results[0].email });   
+  // });
+  // console.log(query.sql);
+
+  res.render('index');   
 });
+
+//http://localhost:3000/checkIfUsernameExists?login=Admin&password=password
+router.get('/checkIfUsernameExists', function(req, res, next) {
+  var post = [req.query.login, req.query.password ];
+  var query = connection.query('select count(*) as c from user where login=? and password=?', post, function (error, results, fields) {
+    if (error) throw error;
+    if(results[0].c == 0){
+        res.redirect('/zaloguj');  
+    }
+    if(results[0].c == 1){
+        res.redirect('account?login='+req.query.login);  
+    }
+     
+  });
+});
+
+//http://localhost:3000/zaloguj
+router.get('/zaloguj', function(req, res, next) {
+    res.render('zaloguj'); 
+});
+
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Express' });
@@ -48,9 +70,6 @@ router.get('/listofusers', function(req, res, next) {
   res.render('listofusers', { title: 'Express' });
 });
 
-router.get('/zaloguj', function(req, res, next) {
-  res.render('zaloguj', { title: 'Express' });
-});
 
 router.get('/zarejestruj', function(req, res, next) {
   res.render('zarejestruj', { title: 'Express' });
