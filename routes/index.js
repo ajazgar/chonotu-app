8afbbe2b957468449843b4517d35e0b898587360
Sebastian Zieja <yowikun@gmail.com>
2018-01-22 19:49:45 +0100

    'buyticket' display price for event

--- routes/index.js
+++ routes/index.js
@@ -131,14 +131,11 @@ router.get('/account', function(req, res, next) {
 
 
 router.all('/buyticket', function(req, res, next) {
-  var values = [];
   var post = [req.query.eventName];
   connection.query('SELECT ticket_price FROM event where eventname=?', post, function (error, row, fields){
-    var cena = {
-        'ticket_price':row[0].ticket_price
-    };
-    values.push(cena);
-res.render('buyticket', { "values": values });
+    var cena = row[0].ticket_price;
+    console.log({"values": cena})
+res.render('buyticket',  {values: cena});
 });
 });
 
